4c1fe57f12aecae680db8d6d96d86e89705d01a6
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-11-26 09:49:30 -0500

    Added code to provide visual feedback to user
    - used click events to change class and state of sort option (Best Match, Highest Rating, Most Reviewed)

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,17 +3,44 @@ import './background_search_desktop.jpg';
 import './background_search_mobile.jpg';
 import './SearchBar.css';
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-}
-
 class SearchBar extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = { term: '', location: '', sortBy: 'best_match' };
+
+      this.handleTermChange = this.handleTermChange.bind(this);
+      this.handleLocationChange = this.handleLocationChange.bind(this);
+
+      this.sortByOptions = {
+        'Best Match': 'best_match',
+        'Highest Rated': 'rating',
+        'Most Reviewed': 'review_count'
+      }
+    }
+
+    handleSortByChange(sortByOption) {
+      this.setState({sortBy: sortByOption})
+    }
+
+    handleTermChange(event) {
+      this.setState({term: event.target.value});
+    }
+
+    handleLocationChange(event) {
+      this.setState({location: event.target.value});
+    }
+
+    getSortByClass(sortByOption) {
+      if (this.state.sortBy === sortByOption) {
+        return 'active';
+      } else { return '';}
+    }
+
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return <li onClick={this.handleSortByChange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
         });
     }
     render() {
@@ -24,8 +51,8 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+          <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
           <a>Let's Go</a>
